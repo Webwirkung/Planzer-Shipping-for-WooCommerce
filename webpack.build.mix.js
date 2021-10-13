adfb5ccf6aca7c20406c4c2e5e77b681357be1b7
Marcin Puszka <marcin.puszka@bold.dev>
2021-10-13 10:35:20 +0200

    Add develop version

---
+++ webpack.build.mix.js
@@ -0,0 +1,21 @@
+/**
+ * Build: Laravel Mix
+ *
+ * @see https://laravel.com/docs/5.8/mix
+ * @author MC
+ */
+
+const mix = require('laravel-mix');
+const path = require('path');
+
+Mix.manifest.refresh = _ => void 0;
+const rootPath = path.join(__dirname);
+const pluginResources = path.join(__dirname, 'resources');
+
+mix.copy(`${rootPath}/woocommerce-planzer.php`, `build/`);
+mix.copyDirectory(`${rootPath}/inc`, `build/inc`);
+mix.copyDirectory(`${rootPath}/src`, `build/src`);
+mix.copyDirectory(`${rootPath}/vendor`, `build/vendor`);
+mix.copyDirectory(`${rootPath}/dist`, `build/dist`);
+mix.copyDirectory(`${pluginResources}/views`, `build/resources/views`);
+mix.copyDirectory(`${pluginResources}/lang`, `build/resources/lang`);
