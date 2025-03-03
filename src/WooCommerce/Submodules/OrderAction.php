bcdc0e1bfb04b7f568ca0b41530034bd26e6bf79
Rafal Orlowski <rafal.orlowski@coditive.com>
2025-03-03 14:44:41 +0100

    Add Tracking Link and translations

--- src/WooCommerce/Submodules/OrderAction.php
+++ src/WooCommerce/Submodules/OrderAction.php
@@ -64,6 +64,8 @@ class OrderAction
       $order_note .= "<br><img src=\"{$package->getGeneratedQRUrl()}\"/>";
       $order_note .= "<br><br>" . __('Reference number:', 'planzer') . " {$order_id}_{$package->getSequenceNumber(0)}";
       $order_note .= "<br><br>" . __('Package number: ', 'planzer') . '<br>' . $package->getQRContentWithoutSuffix();
+      $order_note .= "<br><br><a href='https://tracking.app.planzer.ch/delivery/info?packageNumber=" . $package->getQRContentWithoutSuffix() . "&system=1' target='_blank'>" . __('Track package', 'planzer') . "</a>";
+
       $order->add_order_note($order_note);
     } catch (\Throwable $th) {
       $order->add_order_note('<span style="color:red;font-weight: bold;">Planzer: </span>' . __('There was an error while sending data to Planzer - please try again or check debuglog.', 'planzer'));
@@ -72,6 +74,34 @@ class OrderAction
         $logger = wc_get_logger();
         $logger->error("FATAL ERROR: {$th->getMessage()} in {$th->getFile()} on line {$th->getLine()}", ['source' => 'wc-planzer-shipping']);
       }
+    }
+  }
+
+  /**
+   * @action woocommerce_order_details_after_order_table
+   */
+  public function addTrackingLinkToOrder(WC_Order $order): void
+  {
+    $packageNumber = $order->get_meta('planzer_tracking_code');
+
+    if ($packageNumber) {
+      $trackingUrl = 'https://tracking.app.planzer.ch/delivery/info?packageNumber=' . $packageNumber . '&system=1';
+
+      echo '<p><strong>' . __('Track your package: ', 'planzer') . '</strong> <a href="' . esc_url($trackingUrl) . '" target="_blank">' . __('Click here', 'planzer') . '</a></p>';
+    }
+  }
+
+  /**
+   * @filter woocommerce_email_order_meta
+   */
+  public function addTrackingLinkToEmail(WC_Order $order, $sent_to_admin, $plain_text)
+  {
+    $packageNumber = $order->get_meta('planzer_tracking_code');
+
+    if ($packageNumber && ! $sent_to_admin) {
+      $trackingUrl = 'https://tracking.app.planzer.ch/delivery/info?packageNumber=' . $packageNumber . '&system=1';
+
+      echo '<p><strong>' . __('Track your package: ', 'planzer') . '</strong> <a href="' . esc_url($trackingUrl) . '" target="_blank">' . __('Click here', 'planzer') . '</a></p><br>';
     }
   }
 }
